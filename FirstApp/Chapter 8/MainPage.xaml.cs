5ad4493369936424342f5b0a3774e795145b440b
Joe Swindell <joes@medliminal.com>
2020-01-20 09:54:00 -0500

    Chapter 8 Template setup

---
+++ FirstApp/Chapter 8/MainPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Chapter_8
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private RichEditBox currentRichEditBox;
+        private int counter;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            EntriesListView.Items.Add("super");
+            EntriesListView.Items.Add("extra");
+            EntriesListView.Items.Add("maximum");
+            EntriesListView.Items.Add("advanced");
+            EntriesListView.Items.Add("efficient");
+
+            EntriesListView.Items.Add("super");
+            EntriesListView.Items.Add("extra");
+            EntriesListView.Items.Add("maximum");
+            EntriesListView.Items.Add("advanced");
+            EntriesListView.Items.Add("efficient");
+        }
+
+        private void CreateNewEntryButton_Click(object sender, RoutedEventArgs e)
+        {
+            PivotItem pi = new PivotItem();
+
+            var entryText = String.Format("Entry{0}", rootPivot.Items.Count + 1);
+            pi.Header = entryText;
+            RichEditBox reb = new RichEditBox();
+            reb.HorizontalAlignment = HorizontalAlignment.Stretch;
+            reb.VerticalAlignment = VerticalAlignment.Stretch;
+
+            pi.Content = reb;
+
+            pi.Loaded += PivotItem_Loaded;
+            rootPivot.Items.Add(pi);
+            rootPivot.SelectedIndex = rootPivot.Items.Count - 1;
+        }
+
+        private async void SaveEntryButton_Click(object sender, RoutedEventArgs e)
+        {
+            Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            Windows.Storage.StorageFile sampleFile = await storageFolder.CreateFileAsync("FirstRichEdit.rtf", Windows.Storage.CreationCollisionOption.ReplaceExisting);
+
+            IRandomAccessStream documentStream = await sampleFile.OpenAsync(Windows.Storage.FileAccessMode.ReadWrite);
+            currentRichEditBox.Document.SaveToStream(Windows.UI.Text.TextGetOptions.FormatRtf, documentStream);
+            documentStream.Dispose();
+        }
+
+        private void rootPivot_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Pivot p = sender as Pivot;
+            PivotItem pi = p.SelectedItem as PivotItem;
+            RichEditBox_SetFocus(pi);
+        }
+
+        private void PivotItem_Loaded(object sender, RoutedEventArgs e)
+        {
+            PivotItem pi = sender as PivotItem;
+            RichEditBox_SetFocus(pi);
+        }
+
+        private void RichEditBox_SetFocus(PivotItem pi)
+        {
+            RichEditBox reb = pi.Content as RichEditBox;
+            reb.Focus(FocusState.Keyboard);
+            currentRichEditBox = reb;
+        }
+    }
+}
